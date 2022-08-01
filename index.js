8854447ec8a3b777d2d21a7593ecd0fab6c7e1bd
jkurlansky <105459839+jkurlansky@users.noreply.github.com>
2022-08-01 19:56:56 -0400

    started index.js coding

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 // TODO: Include packages needed for this application
+// const fs = require('fs/promises');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input"
+        message: "What is your Project Title?"
+        name: "title"
+    },
+    {
+        type: "input"
+        message: "something"
+        name: "something"
+    }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer
+    .prompt(questions)
+    .then((data) => { 
+        console.log(data);
+    })
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
